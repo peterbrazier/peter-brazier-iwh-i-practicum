123d26ae8dca454aa0f1cdc464a5a35c1a60eb1b
Peter Brazier <peter.brazier@marketingpod.com>
2025-03-03 17:05:35 +0000

    Added form to submit new movie using axios. Fixed issue with the homepage now showing paged data and sorted the films in alphabetical order ignoring the word 'The'

--- index.js
+++ index.js
@@ -17,25 +17,76 @@ app.get('/', async (req, res) => {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
+
+    let allMovies = [];
+    let after = null;
+
+    const stripArticles = (title) => {
+        return title.replace(/^(a |an |the )/i, '').trim();
+    };
+
     try {
-        const resp = await axios.get(movieUrl, { headers });
-        const data = resp.data.results;
-        console.log(data);
-        res.render('homepage', { title: 'Homepage | Movies', data });
+        do {
+            const url = after ? `${movieUrl}&after=${after}` : movieUrl;
+            const resp = await axios.get(url, { headers });
+            const data = resp.data.results;
+            allMovies = allMovies.concat(data);
+            after = resp.data.paging ? resp.data.paging.next.after : null;
+        } while (after);
+
+        // Sort movies by name in alphabetical order, ignoring articles
+        allMovies.sort((a, b) => {
+            const nameA = stripArticles(a.properties.name.toUpperCase());
+            const nameB = stripArticles(b.properties.name.toUpperCase());
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+
+        res.render('homepage', { title: 'Homepage | Movies', data: allMovies });
     } catch (error) {
         console.error(error);
+        res.status(500).send('Error fetching movies');
     }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
 app.get('/update', (req, res) => {
-    res.render('update', { title: 'Create or Update Movie | Movies' });
+    res.render('update', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update', async (req, res) => {
+    const update = {
+        properties: {
+            name: req.body.name,
+            year: req.body.year,
+            genre: req.body.genre,
+            length: req.body.length,
+            synopsis: req.body.synopsis
+        }
+    };
 
-// * Code for Route 3 goes here
+    const movieId = req.query.id;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        // Create new movie
+        const createMovieUrl = 'https://api.hubapi.com/crm/v3/objects/2-139909312';
+        await axios.post(createMovieUrl, update, { headers });
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error creating movie');
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
